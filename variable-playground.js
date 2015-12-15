e56f386499342e40f1228ffaa87f7ece27d64562
Barry Hall <barrylhall@gmail.com>
2015-12-15 15:18:28 -0500

    Add variable sandbox

---
+++ variable-playground.js
@@ -0,0 +1,19 @@
+// var person = {
+// 	name: 'Barry',
+// 	age: 29
+// };
+
+// function updatePerson (obj) {
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [15, 27];
+function updateGrades(arr) {
+	arr.push(35);
+}
+
+updateGrades(grades);
+console.log(grades);
